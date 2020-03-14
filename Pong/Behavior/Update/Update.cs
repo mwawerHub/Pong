3a1e68f09178a6982d9e200ea2e2de501577c48c
Marcin <marcinwawer8@wp.pl>
2020-03-15 00:22:12 +0100

    Cleaned up Update class

---
+++ Pong/Behavior/Update/Update.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Pong.Abstracts;
+using Pong.Globals;
+
+namespace Pong.Behavior
+{
+    public class Update : Initialize
+    {
+        protected static void UpdateLastPosition(Shape shape){
+            shape.LastXPosition = shape.XStartValue;
+            shape.LastYPosition = shape.YStartValue;
+        }
+
+        protected static void ResetBallPosition(){
+            Ball.XStartValue = (Board.Width + Board.XMargin) / 2;
+            Ball.YStartValue = (Board.Height + Board.YMargin) / 2;
+            Ball.IsAtStartPosition = true;
+        }
+
+        public static void UpdateAll(ConsoleKey key){
+            UpdatePlayer.UpdatePlayerPosition(key);
+            UpdateBall.UpdateBallPosition();
+        }
+    }
+}
---
+++ Pong/Behavior/Update/UpdatePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pong.Globals;
+
+namespace Pong.Behavior
+{
+    public class UpdatePlayer : Update
+    {
+        public static void UpdatePlayerPosition(ConsoleKey key){
+            if (key == 0) return;
+            UpdateLastPosition(Player1);
+            UpdateLastPosition(Player2);
+            Player1.Move(key);
+            Player2.Move(key);
+            State.PlayerNeedsRedraw = true;
+        }
+    }
+}
