b18bf33469a4a261c85038c92da012edb532df34
Marcin <marcinwawer8@wp.pl>
2020-10-05 01:13:38 +0200

    PONG-2 added player vs wall collision

--- Pong/Behavior/CheckPosition.cs
+++ Pong/Behavior/CheckPosition.cs
@@ -9,14 +9,6 @@ namespace Pong.Behavior
             return (y == Board.YMargin + 1 || y == Board.Height - 1);
         }
 
-        public static bool HasPlayer1Scored(int x) {
-            return (x > Board.Width);
-        }
-
-        public static bool HasPlayer2Scored(int x) {
-            return (x < Board.XMargin);
-        }
-
         public static bool HasHitPlayer(Shape player, Ball ball) {
             var offset = -1;
 
@@ -26,6 +18,18 @@ namespace Pong.Behavior
             return ((ball.XStartValue == player.XStartValue + offset)
                 && ball.YStartValue >= player.YStartValue
                 && ball.YStartValue <= player.YStartValue + player.Width);
+        }
+
+        public static bool HasPlayer1Scored(int x) {
+            return (x > Board.Width);
+        }
+
+        public static bool HasPlayer2Scored(int x) {
+            return (x < Board.XMargin);
+        }
+
+        public static bool HasPlayerReachedWall(ConsolePlayer player) {
+            return (player.YStartValue == Board.YMargin || player.YStartValue == Board.Height - player.Height - 3);
         }
     }
 }
--- Pong/Behavior/Update/Update.cs
+++ Pong/Behavior/Update/Update.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using Pong.Abstracts;
+﻿using Pong.Abstracts;
+using System;
 
 namespace Pong.Behavior.Update
 {
     public class Update : Initialize
     {
-        protected static void UpdateLastPosition(Shape shape){
+        protected static void UpdateLastPosition(Shape shape) {
             shape.LastXPosition = shape.XStartValue;
             shape.LastYPosition = shape.YStartValue;
         }
 
-        protected static void ResetBallPosition(){
+        protected static void ResetPosition(Shape shape) {
+            shape.XStartValue = shape.LastXPosition;
+            shape.YStartValue = shape.LastYPosition;
+        }
+
+        protected static void ResetBallPosition() {
             Ball.XStartValue = (Board.Width + Board.XMargin) / 2;
             Ball.YStartValue = (Board.Height + Board.YMargin) / 2;
             Ball.IsAtStartPosition = true;
         }
 
-        public static void UpdateAll(ConsoleKey key){
+        public static void UpdateAll(ConsoleKey key) {
             UpdatePlayer.UpdatePlayerPosition(key);
             UpdateBall.UpdateBallPosition();
         }
