e422e0910c036d85d5da468438e7cea6757f301d
Marcin <marcinwawer8@wp.pl>
2020-10-05 00:42:04 +0200

    PONG-5 Fixed start instructions position

--- Pong/Abstracts/Introduction.cs
+++ Pong/Abstracts/Introduction.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Pong.Abstracts 
+namespace Pong.Abstracts
 {
     public abstract class Introduction
     {
-        public static void DrawInstructions(){
-            Console.SetCursorPosition((Board.Width+Board.XMargin)/2, (Board.Height + Board.YMargin) / 2);
+        public static void DrawInstructions() {
+            Console.SetCursorPosition((Board.Width - Board.XMargin) / 2, (Board.Height - Board.YMargin) / 2 - 3);
+            Console.WriteLine("Instructions:");
+            Console.SetCursorPosition((Board.Width - Board.XMargin) / 2, (Board.Height - Board.YMargin) / 2);
             Console.WriteLine("Player 1 - W S A D");
-            Console.SetCursorPosition((Board.Width + Board.XMargin) / 2, (Board.Height + Board.YMargin) / 2 + 1);
+            Console.SetCursorPosition((Board.Width - Board.XMargin) / 2, (Board.Height - Board.YMargin) / 2 + 1);
             Console.WriteLine("Player 2 - Arrow keys");
+            Console.SetCursorPosition((Board.Width - Board.XMargin) / 2, (Board.Height - Board.YMargin) / 2 + 4);
+            Console.WriteLine("Have Fun!");
         }
-
-
     }
 }
